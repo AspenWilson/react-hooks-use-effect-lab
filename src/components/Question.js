395e67f7c77efa7ad13a15cf55e8255c54a4d038
Aspen Wilson <aspen.schell@gmail.com>
2023-04-16 19:44:15 -0700

    completed lab

--- src/components/Question.js
+++ src/components/Question.js
@@ -1,9 +1,24 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
 function Question({ question, onAnswered }) {
   const [timeRemaining, setTimeRemaining] = useState(10);
 
   // add useEffect code
+  useEffect(() => {
+    const countdown = setInterval(() => {
+      setTimeRemaining(timeRemaining => timeRemaining - 1);
+    },1000);
+    return () => {
+      clearInterval(countdown)
+    }
+  },[])
+
+  useEffect(() => {
+    if (timeRemaining === 0){
+      onAnswered(false)
+      setTimeRemaining(10)
+    }
+  }, [timeRemaining])
 
   function handleAnswer(isCorrect) {
     setTimeRemaining(10);
